42678ebfc0fad94c889f19bf9289622e75a8fda8
Your Name <you@example.com>
2025-03-20 18:10:08 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -129,7 +129,7 @@ int parentesisBalanceados(char *cadena) {
    }
    if (i % 2) return 0 ;
    
-   for(i = 0 ; cadena[i] / 2 ; pop(stack_aux), i++) {
+   for(int d = i, i = 0 ; d < i / 2 ; pop(stack_aux), i++) {
       dato = top(stack_aux) ;
       switch (cadena[i]) {
          case '(' : {
